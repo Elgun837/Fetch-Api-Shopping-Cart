54514f5516d266e140aa8bf0f3a57fcdebe9c45d
elgun837 <elgun.xalilov@bk.ru>
2022-11-17 17:53:22 +0400

    Js writting

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const cartBtn = document.querySelector(".cart-btn");
+const clearCarBtn = document.querySelector(".btn-clear");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".total-value");
+const cartContent = document.querySelector(".cart-list");
+const productsDom = document.querySelector("#products-dom");
